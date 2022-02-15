0b9f4d329f23535e238a8bae535f6c2ea529d048
李静 <453019202@qq.com>
2022-02-15 19:49:32 +0800

    项目结构调整

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -1,49 +1,20 @@
 // pages/home-music/index.js
-import { musicApi } from '../../api/music'
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        musiLists: [],
-        name: "zs"
+
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        this.loadMore()
-    },
-    async loadMore(first) {
-        let len = this.data.musiLists.length
-        wx.showLoading({
-            title: '加载中',
-        })
-        let res = await musicApi.getBaseData(len)
-        wx.hideLoading()
-        if (res.hasMore) {
-            if (first) {
-                this.setData({
-                    musiLists: this.data.musiLists.concat(res.data)
-                })
-            } else {
-                this.setData({
-                    musiLists: res.data
-                })
-            }
-        }
-    },
-    jumpDetail(e){
 
-        
-        wx.navigateTo({
-          url: `/pages/detail-video/index?id=${e.currentTarget.dataset.id}`,
-        })
     },
-    onReachBottom: async function () {
-    },
+
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
@@ -55,6 +26,7 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
+
     },
 
     /**
@@ -68,26 +40,27 @@ Page({
      * 生命周期函数--监听页面卸载
      */
     onUnload: function () {
+
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: async function () {
-        await this.loadMore(false)
-        wx.stopPullDownRefresh()
+    onPullDownRefresh: function () {
+
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
-        this.loadMore(true)
+
     },
+
     /**
      * 用户点击右上角分享
      */
     onShareAppMessage: function () {
 
-    },
+    }
 })
--- pages/home-video/index.js
+++ pages/home-video/index.js
@@ -1,20 +1,49 @@
-// pages/home-video/index.js
+// pages/home-music/index.js
+import { musicApi } from '../../api/music'
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        musiLists: [],
+        name: "zs"
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        this.loadMore()
     },
+    async loadMore(first) {
+        let len = this.data.musiLists.length
+        wx.showLoading({
+            title: '加载中',
+        })
+        let res = await musicApi.getBaseData(len)
+        wx.hideLoading()
+        if (res.hasMore) {
+            if (first) {
+                this.setData({
+                    musiLists: this.data.musiLists.concat(res.data)
+                })
+            } else {
+                this.setData({
+                    musiLists: res.data
+                })
+            }
+        }
+    },
+    jumpDetail(e){
 
+        
+        wx.navigateTo({
+          url: `/pages/detail-video/index?id=${e.currentTarget.dataset.id}`,
+        })
+    },
+    onReachBottom: async function () {
+    },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
@@ -26,7 +55,6 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
     },
 
     /**
@@ -40,27 +68,26 @@ Page({
      * 生命周期函数--监听页面卸载
      */
     onUnload: function () {
-
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {
-
+    onPullDownRefresh: async function () {
+        await this.loadMore(false)
+        wx.stopPullDownRefresh()
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
-
+        this.loadMore(true)
     },
-
     /**
      * 用户点击右上角分享
      */
     onShareAppMessage: function () {
 
-    }
+    },
 })
